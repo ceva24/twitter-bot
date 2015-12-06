0fd867995c990a257ab061efa194995a6b8d36dd
Ceva24 <ceva24@outlook.com>
2015-12-06 18:14:15 +0000

    updated schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,2 @@
 CREATE DATABASE symphonia;
-
 CREATE TABLE symphonia.quote (id INT NOT NULL AUTO_INCREMENT, text VARCHAR(140) NOT NULL, sequence_no INT NOT NULL UNIQUE, is_tweeted BOOLEAN NOT NULL, PRIMARY KEY (id));
