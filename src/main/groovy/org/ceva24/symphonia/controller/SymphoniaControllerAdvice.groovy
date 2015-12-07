283a585a66c21e85e56c9fcd4c4446d60ae0a273
Ceva24 <ceva24@outlook.com>
2015-12-07 00:35:10 +0000

    changed wait period name to quiet period

--- src/main/groovy/org/ceva24/symphonia/controller/SymphoniaControllerAdvice.groovy
+++ src/main/groovy/org/ceva24/symphonia/controller/SymphoniaControllerAdvice.groovy
@@ -2,7 +2,6 @@ package org.ceva24.symphonia.controller
 
 import org.ceva24.symphonia.exception.WaitPeriodException
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.MessageSource
 import org.springframework.http.HttpStatus
 import org.springframework.social.DuplicateStatusException
@@ -17,13 +16,10 @@ class SymphoniaControllerAdvice {
     @Autowired
     MessageSource messageSource
 
-    @Value('${org.ceva24.symphonia.quote.wait-period.seconds}')
-    Integer waitPeriod
-
     @ExceptionHandler(WaitPeriodException)
     def inWaitPeriod(HttpServletResponse response, WaitPeriodException e) {
 
-        def message = messageSource.getMessage 'org.ceva24.symphonia.in-wait-period', [waitPeriod, e.secondsRemaining] as Object[], Locale.default
+        def message = messageSource.getMessage 'org.ceva24.symphonia.in-quiet-period', [e.secondsRemaining] as Object[], Locale.default
 
         response.sendError HttpStatus.BAD_REQUEST.value(), message
     }
