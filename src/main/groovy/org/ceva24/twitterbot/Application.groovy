ff707bb26a711bc397f73bef06603990237d1a0e
Ceva24 <ceva24@outlook.com>
2015-12-12 13:57:54 +0000

    removed TODO

--- src/main/groovy/org/ceva24/twitterbot/Application.groovy
+++ src/main/groovy/org/ceva24/twitterbot/Application.groovy
@@ -8,8 +8,6 @@ import org.springframework.boot.context.web.SpringBootServletInitializer
 @SpringBootApplication
 class Application extends SpringBootServletInitializer {
 
-    // TODO unit tests (test can tweet straight after duplicate error)
-
     static def main(def args) {
 
         SpringApplication.run Application, args as String[]
