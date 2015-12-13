8110ee144a04fe309269d4c0001af62fffae7d9a
Ceva24 <ceva24@outlook.com>
2015-12-13 03:09:07 +0000

    syntax changes

--- src/test/groovy/org/ceva24/twitterbot/service/TwitterBotServiceIntegrationSpec.groovy
+++ src/test/groovy/org/ceva24/twitterbot/service/TwitterBotServiceIntegrationSpec.groovy
@@ -34,10 +34,10 @@ class TwitterBotServiceIntegrationSpec extends Specification {
     def 'an exception thrown when activating the downtime period performs a rollback on twitter status'() {
 
         setup:
-        testTwitterStatusRepository.save(new TwitterStatus(id: 1, text: 'test1', sequenceNo: 1))
+        testTwitterStatusRepository.save new TwitterStatus(id: 1, text: 'test1', sequenceNo: 1)
 
         and:
-        twitterBotService.configRepository = Mock(ConfigRepository)
+        twitterBotService.configRepository = Mock ConfigRepository
         twitterBotService.configRepository.setActiveOnFor(_, _) >> { throw new RuntimeException() }
 
         when:
@@ -53,8 +53,8 @@ class TwitterBotServiceIntegrationSpec extends Specification {
     def 'an exception thrown when sending a tweet performs a rollback on twitter status and config'() {
 
         setup:
-        testTwitterStatusRepository.save(new TwitterStatus(id: 1, text: 'test1', sequenceNo: 1))
-        testConfigRepository.save(new Config(id: Config.ConfigKey.DOWNTIME))
+        testTwitterStatusRepository.save new TwitterStatus(id: 1, text: 'test1', sequenceNo: 1)
+        testConfigRepository.save new Config(id: Config.ConfigKey.DOWNTIME)
 
         and:
         twitterBotService.tweetService = Mock(TweetService)
